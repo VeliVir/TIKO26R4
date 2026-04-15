116c98c239fd12a12385add7d779bdf0f1490200
VeliVir <velivirtanen13@gmail.com>
2026-04-16 01:24:10 +0300

    Creted db_connection files + gitignore
    Signed-off-by: VeliVir <velivirtanen13@gmail.com>

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// Tietokantayhteyden asetukset, ei tallenneta versionhallintaan (.gitignore) 
+$host = "";
+$port = "";
+$dbname = "";
+$user = ""; 
+$password = "";
+
+$y_tiedot = "host=$host port=$port dbname=$dbname user=$user password=$password";
+
+?>
---
+++ sql/db_connection.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'config.php';
+
+$yhteys = pg_connect($y_tiedot);
+
+if (!$yhteys) {
+    echo "Ei yhteyttä tietokantaan<br>";
+}
+?>
+
+
