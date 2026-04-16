6a8b2406f71b431f8fd7336a95ea256b2daf763b
VeliVir <velivirtanen13@gmail.com>
2026-04-16 03:50:18 +0300

    modified README.md
    Signed-off-by: VeliVir <velivirtanen13@gmail.com>

--- temp_config.php
+++ temp_config.php
@@ -1,4 +1,4 @@
 <?php
 // Tietokantayhteyden asetuksien pohja, kopioi config.php nimiseen tiedostoon ja täytä omilla tiedoillasi.
-$y_tiedot = "dbname=$dbname user=$user password=$password";
+$y_tiedot = "dbname=placeholder user=placeholder password=placeholder";
 ?>
